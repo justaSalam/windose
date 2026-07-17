8126548c499c2dd541fb03277e7c1c29f205adab
swhx <salamlex147@gmail.com>
2026-07-17 21:25:53 +0200

    STUPID FUCKING GITIGNORE

--- obj/Debug/net10.0/win-x64/Windose.AssemblyInfo.cs
+++ obj/Debug/net10.0/win-x64/Windose.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Windose")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f499a77c277ca3ad91bd5dc0c97431bd2aedeb42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ad911c96430349b60d658dcfa47534b885a49c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Windose")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Windose")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
